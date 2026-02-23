9eef9e249e1cccad9b9ef85c61ca69c48c0f544f
Neel-Asher <neel.asher20@gmail.com>
2026-02-23 14:16:25 +0530

    Coded to initialize the guessing game and set up the game configuration

---
+++ GuessingApp.java
@@ -0,0 +1,19 @@
+/**
+* GuessingApp - Use Case 1: Game Initialization
+* 
+* This class serves as the application entry point.
+* It initializes the game configuration and displays game rules.
+* 
+* No user input or gameplay logic is implemented at this stage.
+*
+* @author Neel Asher
+* @version 1.0
+*/
+
+public class GuessingApp {
+	public static void main (String[] args) {
+		System.out.println("Welcome to the Gussing App!");
+		GameConfig gameConfig = new GameConfig();
+		gameConfig.showRules();
+	}
+}
